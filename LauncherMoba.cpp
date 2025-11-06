b2fe6885905200b707d7444a9632b75e1f8b8d0c
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2025-11-06 18:00:47 +0100

    finish configuration sfml

--- LauncherMoba.cpp
+++ LauncherMoba.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
+
+    return 0;
 }
